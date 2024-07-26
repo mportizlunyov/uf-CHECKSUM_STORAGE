2e0a78a835d0b86bcec20bdfe5dc11a8f2ef107b
Mikhail P. Ortiz-Lunyov <108703641+mportizlunyov@users.noreply.github.com>
2024-07-25 22:07:49 -0500

    Update 00_CHECKSUM_CHECKER.sh

--- Update_Full-UNIX/00_CHECKSUM_CHECKER.sh
+++ Update_Full-UNIX/00_CHECKSUM_CHECKER.sh
@@ -24,15 +24,13 @@ case $2 in
         case $3 in
             "true")
                 echo "* DOWNLOADING using CURL [--insecure !!!]"
-                curl --insecure --remote-name --silent https://raw.githubusercontent.com/mportizlunyov/uf-CHECKSUM_STORAGE/main/Update_Full-UNIX/latest/update_full-unix-$1.sha256sum &
-                curl --insecure --remote-name --silent https://raw.githubusercontent.com/mportizlunyov/uf-CHECKSUM_STORAGE/main/Update_Full-UNIX/latest/update_full-unix-$1.sha512sum &
-                wait
+                curl --insecure --remote-name --silent https://raw.githubusercontent.com/mportizlunyov/uf-CHECKSUM_STORAGE/main/Update_Full-UNIX/latest/update_full-unix-$1.sha256sum
+                curl --insecure --remote-name --silent https://raw.githubusercontent.com/mportizlunyov/uf-CHECKSUM_STORAGE/main/Update_Full-UNIX/latest/update_full-unix-$1.sha512sum
                 ;;
             "false")
                 echo "* DOWNLOADING using CURL"
-                curl --remote-name --silent https://raw.githubusercontent.com/mportizlunyov/uf-CHECKSUM_STORAGE/main/Update_Full-UNIX/latest/update_full-unix-$1.sha256sum &
-                curl --remote-name --silent https://raw.githubusercontent.com/mportizlunyov/uf-CHECKSUM_STORAGE/main/Update_Full-UNIX/latest/update_full-unix-$1.sha512sum &
-                wait
+                curl --remote-name --silent https://raw.githubusercontent.com/mportizlunyov/uf-CHECKSUM_STORAGE/main/Update_Full-UNIX/latest/update_full-unix-$1.sha256sum
+                curl --remote-name --silent https://raw.githubusercontent.com/mportizlunyov/uf-CHECKSUM_STORAGE/main/Update_Full-UNIX/latest/update_full-unix-$1.sha512sum
                 ;;
             *)
                 printf "\t!!! INTERNAL PROGRAM ERROR !!!\n"
@@ -42,9 +40,8 @@ case $2 in
         ;;
     "wget")
         echo "* DOWNLOADING using WGET"
-        wget --quiet https://raw.githubusercontent.com/mportizlunyov/uf-CHECKSUM_STORAGE/main/Update_Full-UNIX/latest/update_full-unix-$1.sha256sum &
-        wget --quiet https://raw.githubusercontent.com/mportizlunyov/uf-CHECKSUM_STORAGE/main/Update_Full-UNIX/latest/update_full-unix-$1.sha512sum &
-        wait
+        wget --quiet https://raw.githubusercontent.com/mportizlunyov/uf-CHECKSUM_STORAGE/main/Update_Full-UNIX/latest/update_full-unix-$1.sha256sum
+        wget --quiet https://raw.githubusercontent.com/mportizlunyov/uf-CHECKSUM_STORAGE/main/Update_Full-UNIX/latest/update_full-unix-$1.sha512sum
         ;;
     *)
         printf "\n\t^ Neither CURL nor WGET exists, cannot continue!!!\n"
@@ -54,19 +51,16 @@ esac
 # If checksum files are not rpesent
 if [ ! -f "./update_full-unix-$1.sha256sum" ] && [ ! -f "update_full-unix-$1.sha512sum" ] ; then
     printf "\n\t ^ Checksums FAILED to download using $2, check connection!!\n"
-    rm ./update_full-unix-$1.sha256sum > /dev/null 2>&1 &
-    rm ./update_full-unix-$1.sha512sum > /dev/null 2>&1 &
-    wait
+    rm ./update_full-unix-$1.sha256sum > /dev/null 2>&1
+    rm ./update_full-unix-$1.sha512sum > /dev/null 2>&1
     exit 1
 fi
 # Format downloaded checksums
-echo "$(cat ./update_full-unix-$1.sha256sum | cut -d ' ' -f 1)" > ./update_full-unix-$1.sha256sum &
-echo "$(cat ./update_full-unix-$1.sha512sum | cut -d ' ' -f 1)" > ./update_full-unix-$1.sha512sum &
-wait
+echo "$(cat ./update_full-unix-$1.sha256sum | cut -d ' ' -f 1)" > ./update_full-unix-$1.sha256sum
+echo "$(cat ./update_full-unix-$1.sha512sum | cut -d ' ' -f 1)" > ./update_full-unix-$1.sha512sum
 # Format actual checksums
-echo "$(sha256sum ./update_full-unix.sh | cut -d ' ' -f 1)" > ./tempfile_ACTUAL256 &
-echo "$(sha512sum ./update_full-unix.sh | cut -d ' ' -f 1)" > ./tempfile_ACTUAL512 &
-wait
+echo "$(sha256sum ./update_full-unix.sh | cut -d ' ' -f 1)" > ./tempfile_ACTUAL256
+echo "$(sha512sum ./update_full-unix.sh | cut -d ' ' -f 1)" > ./tempfile_ACTUAL512
 if [ "$(cat ./tempfile_ACTUAL256)" = "" ] || [ "$( cat ./tempfile_ACTUAL512)" = "" ] ; then
     echo "$(cksum -a sha256 -q ./update_full-unix.sh)" > ./tempfile_ACTUAL256 &
     echo "$(cksum -a sha512 -q ./update_full-unix.sh)" > ./tempfile_ACTUAL512 &
@@ -81,9 +75,8 @@ else
     exit 1
 fi
 # If everything runs as normal
-rm ./update_full-unix-$1.sha256sum > /dev/null 2>&1 &
-rm ./update_full-unix-$1.sha512sum > /dev/null 2>&1 &
-rm ./tempfile_ACTUAL256 > /dev/null 2>&1 &
-rm ./tempfile_ACTUAL512 > /dev/null 2>&1 &
-wait
+rm ./update_full-unix-$1.sha256sum > /dev/null 2>&1
+rm ./update_full-unix-$1.sha512sum > /dev/null 2>&1
+rm ./tempfile_ACTUAL256 > /dev/null 2>&1
+rm ./tempfile_ACTUAL512 > /dev/null 2>&1
 exit 0
